c40da65c4255641d5425a438eda29da81845d73b
Gianni <gianni.lodetti@hotmail.com>
2023-02-13 22:58:26 +0100

    checkpoint

--- laravel_tutorial/blog/resources/views/posts.blade.php
+++ laravel_tutorial/blog/resources/views/posts.blade.php
@@ -2,7 +2,23 @@
 
 <x-layout>
     <x-slot name="content">
-        @foreach($posts as $post)
+        @include ('_posts-header') <!-- include partial view -->
+
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+            <x-post-featured-card/>
+
+            <div class="lg:grid lg:grid-cols-2">
+                <x-post-card/>
+                <x-post-card/>
+            </div>
+
+            <div class="lg:grid lg:grid-cols-3">
+                <x-post-card/>
+                <x-post-card/>
+                <x-post-card/>
+            </div>
+        </main>
+        {{-- @foreach($posts as $post)
             <article>
                 <h1>
                     <a href="/posts/{{$post->id}}">
@@ -15,7 +31,7 @@
                     {{$post->excerpt}}
                 </div>    
             </article>
-        @endforeach
+        @endforeach --}}
     </x-slot>
 </x-layout>
 
